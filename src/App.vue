69beb4a0cfaa6839302329537695cf2fb342e346
Atanazio95 <atanaziojeorge@gmail.com>
2021-07-14 22:02:10 -0300

    novidade nova

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,74 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <v-card
+    class="mx-auto overflow-hidden"
+    height="400"
+  >
+    <v-app-bar
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Pokemons</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+          
+           <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-arrow-up-bold-box-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Geral</v-list-item-title>
+          </v-list-item>
+
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-call-split</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Evoluções</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-domain</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Por tipo</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+  
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
   }
-}
+</script>
+
+<style lang="scss">
+
 </style>
